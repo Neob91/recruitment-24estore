487100a40902518642e376d163103069b02247be
Andrzej Bartosiński <neob91@gmail.com>
2020-01-25 19:03:50 +0100

    Add saga tests and readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Setup
+
+Go to project directory and run `npm install`.
+To start the project, run `npm start`.
+To run tests, run `npm test`.
---
+++ src/store/sagas.spec.ts
@@ -0,0 +1,30 @@
+import { fetchCurrencyExchange } from './actions';
+import { expectSaga } from 'redux-saga-test-plan';
+import { getType } from 'typesafe-actions';
+
+import { saga } from './sagas';
+
+jest.mock('@/services/api', () => ({
+  fetchCurrencyExchange: async () => testData
+}));
+
+const testData = [
+  { code: 'USD', currency: 'US dollar', mid: 3.95 },
+  { code: 'AUD', currency: 'Australian dollar', mid: 2.64 },
+  { code: 'EUR', currency: 'Euro', mid: 4.4 },
+];
+
+describe('saga', () => {
+  test('triggers API call on request', () => {
+    return expectSaga(saga)
+      .put(fetchCurrencyExchange.success({ data: testData }))
+      .dispatch(fetchCurrencyExchange.request())
+      .run();
+  });
+
+  test('does nothing without any action', () => {
+    return expectSaga(saga)
+      .not.put.like({ action: { type: getType(fetchCurrencyExchange.success) } })
+      .run();
+  });
+});
